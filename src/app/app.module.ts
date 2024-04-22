e7270865fdea4782925b184d67537c920a38f838
ranniarcanjo <ranniarcanjo@gmail.com>
2024-04-22 09:09:54 -0300

    criacao de pagina para aprendizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { BaseUiComponent } from './base-ui/base-ui.component';
+import { UsersComponent } from './pages/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    BaseUiComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
